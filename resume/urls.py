70b4023ff3b16bb1f2c79d3b31c10d1fc2cf8cb0
saeid shabani <django_saeid.sh@proton.me>
2024-10-13 13:03:11 +0330

    create experience seg

--- resume/urls.py
+++ resume/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('about/',views.about,name='about'),
     path('projects/',views.projects,name='projects'),
+    path('experiences/',views.experiences,name='experiences'),
 
 ]
--- resume/views.py
+++ resume/views.py
@@ -8,12 +8,26 @@ def about(request):
     return render(request,'about.html')
 
 def projects(request):
+
     projects_show = [
         {'title':'blog',
          'path':'images/blog.png'},
 
     ]
     return render(request,'projects.html',{'projects_show':projects_show})
+
+
+def experiences(request):
+    experiences=[
+        {'company_name':'abc',
+         'position':'python developer'},
+    ]
+    return render(request,'experience.html',{'experiences':experiences})
+
+
+
+
+
 
 
 
